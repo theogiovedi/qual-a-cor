6e381584e49c8ffbc7f32e3bd72e4b355d9b1ec2
Théo Giovedi <tmgiovedi@gmail.com>
2023-03-02 20:39:47 -0300

    fix: icons now follow the respective mode

--- script.js
+++ script.js
@@ -10,9 +10,13 @@ function toggleDarkMode() {
     if (fg == white && bg == black) {
         root.style.setProperty("--foreground-color", black);
         root.style.setProperty("--background-color", white);
+        darkMode.classList.toggle("bi-moon-stars-fill");
+        darkMode.classList.toggle("bi-brightness-high-fill");
     } else {
         root.style.setProperty("--foreground-color", white);
         root.style.setProperty("--background-color", black);
+        darkMode.classList.toggle("bi-moon-stars-fill");
+        darkMode.classList.toggle("bi-brightness-high-fill");
     }
 }
 
