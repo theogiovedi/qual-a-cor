fa814b408a806c5f2331d62f1ae053f8ca9a982e
Théo Giovedi <tmgiovedi@gmail.com>
2023-03-02 20:49:43 -0300

    feat: logo now changes from light to dark and vice-versa

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const darkMode = document.getElementById("toggle-dark-mode");
+const logo = document.getElementById("navlogo");
 
 function toggleDarkMode() {
     const root = document.querySelector(":root");
@@ -7,16 +8,20 @@ function toggleDarkMode() {
     const fg = style.getPropertyValue("--foreground-color");
     const black = style.getPropertyValue("--black");
     const white = style.getPropertyValue("--white");
+    // To Light Mode
     if (fg == white && bg == black) {
         root.style.setProperty("--foreground-color", black);
         root.style.setProperty("--background-color", white);
         darkMode.classList.toggle("bi-moon-stars-fill");
         darkMode.classList.toggle("bi-brightness-high-fill");
+        logo.src = "navlogo-light.png";
+    // To Dark Mode
     } else {
         root.style.setProperty("--foreground-color", white);
         root.style.setProperty("--background-color", black);
         darkMode.classList.toggle("bi-moon-stars-fill");
         darkMode.classList.toggle("bi-brightness-high-fill");
+        logo.src = "navlogo-dark.png";
     }
 }
 
