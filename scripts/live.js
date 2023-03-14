68f441c0b546e96c5a797fa747a3e8e84263e858
Théo Giovedi <tmgiovedi@gmail.com>
2023-03-14 10:00:20 -0300

    refactor: optimizations for filters

--- scripts/live.js
+++ scripts/live.js
@@ -87,7 +87,9 @@ function setup() {
             return;
         } else {
             cam = createCapture({
-                facingMode: { ideal: "environment" },
+                video: {
+                    facingMode: { ideal: "environment" },
+                }
             });
             cam.hide();
             canvasWidth = windowWidth - 40;
@@ -123,33 +125,132 @@ function draw() {
         return
     } else if (type == "prot") {
         for (let i = 0; i < camFrame.pixels.length; i += 4) {
-            const r = camFrame.pixels[i];
-            const g = camFrame.pixels[i + 1];
-            const b = camFrame.pixels[i + 2];
-            const rgbSimulated = normalToProtanomaly(r, g, b);
-            camFrame.pixels[i] = rgbSimulated[0];
-            camFrame.pixels[i + 1] = rgbSimulated[1];
-            camFrame.pixels[i + 2] = rgbSimulated[2];
+            const rgb = [
+                [camFrame.pixels[i] / 255],
+                [camFrame.pixels[i + 1] / 255],
+                [camFrame.pixels[i + 2] / 255]
+            ]
+
+            const matrix = [
+                [0.152286, 1.052583, -0.204868],
+                [0.114503, 0.786281, 0.099216],
+                [-0.003882, -0.048116, 1.051998]
+            ]
+
+            const simulatedRgb = [
+                [(rgb[0][0] * matrix[0][0]) + (rgb[1][0] * matrix[0][1]) + (rgb[2][0] * matrix[0][2])],
+                [(rgb[0][0] * matrix[1][0]) + (rgb[1][0] * matrix[1][1]) + (rgb[2][0] * matrix[1][2])],
+                [(rgb[0][0] * matrix[2][0]) + (rgb[1][0] * matrix[2][1]) + (rgb[2][0] * matrix[2][2])]
+            ]
+
+            if (simulatedRgb[0][0] < 0) {
+                simulatedRgb[0][0] = 0;
+            } else if (simulatedRgb[0][0] > 1) {
+                simulatedRgb[0][0] = 1;
+            }
+
+            if (simulatedRgb[1][0] < 0) {
+                simulatedRgb[1][0] = 0;
+            } else if (simulatedRgb[1][0] > 1) {
+                simulatedRgb[1][0] = 1;
+            }
+
+
+            if (simulatedRgb[2][0] < 0) {
+                simulatedRgb[2][0] = 0;
+            } else if (simulatedRgb[2][0] > 1) {
+                simulatedRgb[2][0] = 1;
+            }
+
+            camFrame.pixels[i] = Math.floor(simulatedRgb[0][0] * 255);
+            camFrame.pixels[i + 1] = Math.floor(simulatedRgb[1][0] * 255);
+            camFrame.pixels[i + 2] = Math.floor(simulatedRgb[2][0] * 255);
         }
     } else if (type == "deut") {
         for (let i = 0; i < camFrame.pixels.length; i += 4) {
-            const r = camFrame.pixels[i];
-            const g = camFrame.pixels[i + 1];
-            const b = camFrame.pixels[i + 2];
-            const rgbSimulated = normalToDeuteranomaly(r, g, b);
-            camFrame.pixels[i] = rgbSimulated[0];
-            camFrame.pixels[i + 1] = rgbSimulated[1];
-            camFrame.pixels[i + 2] = rgbSimulated[2];
+            const rgb = [
+                [camFrame.pixels[i] / 255],
+                [camFrame.pixels[i + 1] / 255],
+                [camFrame.pixels[i + 2] / 255]
+            ]
+
+            const matrix = [
+                [0.367322, 0.860646, -0.227968],
+                [0.280085, 0.672501, 0.047413],
+                [-0.011820, 0.042940, 0.968881]
+            ]
+
+            const simulatedRgb = [
+                [(rgb[0][0] * matrix[0][0]) + (rgb[1][0] * matrix[0][1]) + (rgb[2][0] * matrix[0][2])],
+                [(rgb[0][0] * matrix[1][0]) + (rgb[1][0] * matrix[1][1]) + (rgb[2][0] * matrix[1][2])],
+                [(rgb[0][0] * matrix[2][0]) + (rgb[1][0] * matrix[2][1]) + (rgb[2][0] * matrix[2][2])]
+            ]
+
+            if (simulatedRgb[0][0] < 0) {
+                simulatedRgb[0][0] = 0;
+            } else if (simulatedRgb[0][0] > 1) {
+                simulatedRgb[0][0] = 1;
+            }
+
+            if (simulatedRgb[1][0] < 0) {
+                simulatedRgb[1][0] = 0;
+            } else if (simulatedRgb[1][0] > 1) {
+                simulatedRgb[1][0] = 1;
+            }
+
+
+            if (simulatedRgb[2][0] < 0) {
+                simulatedRgb[2][0] = 0;
+            } else if (simulatedRgb[2][0] > 1) {
+                simulatedRgb[2][0] = 1;
+            }
+
+            camFrame.pixels[i] = Math.floor(simulatedRgb[0][0] * 255);
+            camFrame.pixels[i + 1] = Math.floor(simulatedRgb[1][0] * 255);
+            camFrame.pixels[i + 2] = Math.floor(simulatedRgb[2][0] * 255);
         }
     } else if (type == "trit") {
         for (let i = 0; i < camFrame.pixels.length; i += 4) {
-            const r = camFrame.pixels[i];
-            const g = camFrame.pixels[i + 1];
-            const b = camFrame.pixels[i + 2];
-            const rgbSimulated = normalToTritanomaly(r, g, b);
-            camFrame.pixels[i] = rgbSimulated[0];
-            camFrame.pixels[i + 1] = rgbSimulated[1];
-            camFrame.pixels[i + 2] = rgbSimulated[2];
+            const rgb = [
+                [camFrame.pixels[i] / 255],
+                [camFrame.pixels[i + 1] / 255],
+                [camFrame.pixels[i + 2] / 255]
+            ]
+
+            const matrix = [
+                [1.255528, -0.076749, -0.178779],
+                [-0.078411, 0.930809, 0.147602],
+                [0.004733, 0.691367, 0.303900]
+            ]
+
+            const simulatedRgb = [
+                [(rgb[0][0] * matrix[0][0]) + (rgb[1][0] * matrix[0][1]) + (rgb[2][0] * matrix[0][2])],
+                [(rgb[0][0] * matrix[1][0]) + (rgb[1][0] * matrix[1][1]) + (rgb[2][0] * matrix[1][2])],
+                [(rgb[0][0] * matrix[2][0]) + (rgb[1][0] * matrix[2][1]) + (rgb[2][0] * matrix[2][2])]
+            ]
+
+            if (simulatedRgb[0][0] < 0) {
+                simulatedRgb[0][0] = 0;
+            } else if (simulatedRgb[0][0] > 1) {
+                simulatedRgb[0][0] = 1;
+            }
+
+            if (simulatedRgb[1][0] < 0) {
+                simulatedRgb[1][0] = 0;
+            } else if (simulatedRgb[1][0] > 1) {
+                simulatedRgb[1][0] = 1;
+            }
+
+
+            if (simulatedRgb[2][0] < 0) {
+                simulatedRgb[2][0] = 0;
+            } else if (simulatedRgb[2][0] > 1) {
+                simulatedRgb[2][0] = 1;
+            }
+
+            camFrame.pixels[i] = Math.floor(simulatedRgb[0][0] * 255);
+            camFrame.pixels[i + 1] = Math.floor(simulatedRgb[1][0] * 255);
+            camFrame.pixels[i + 2] = Math.floor(simulatedRgb[2][0] * 255);
         }
     }
 
